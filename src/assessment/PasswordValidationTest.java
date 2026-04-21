b7a3fdb498c071415f07312cc703f972dc8906f5
ffa <fellahkoickal@gmail.com>
2026-04-21 17:56:12 +0530

    JUnit Test Cases Assessment

---
+++ src/assessment/PasswordValidationTest.java
@@ -0,0 +1,27 @@
+package assessment;
+
+import assessment.InvalidPasswordException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PasswordValidationTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void testShortPassword() {
+        InvalidPasswordException e = assertThrows(InvalidPasswordException.class, () -> PasswordValidation.validatePassword("xyz23"));
+        assertEquals("Password length must not be less than 8", e.getMessage());
+    }
+
+    @Test
+    void testPasswordNoDigit() {
+        InvalidPasswordException e = assertThrows(InvalidPasswordException.class, () -> PasswordValidation.validatePassword("abcyzghjko"));
+        assertEquals("Password must contain at least one digit", e.getMessage());
+    }
+}
